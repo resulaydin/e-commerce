dac1188f9a9273ade001399f4fca65f042be1a02
resul aydın <bm.resulaydin@gmail.com>
2023-05-29 15:29:34 +0300

    some bugfix handled and glide.js library has been used

---
+++ js/glide.js
@@ -0,0 +1,44 @@
+// bu kullanım ile sadece 1 tane slide item gösterilecektir.
+// new Glide(".glide").mount();
+
+// Product
+const config_1 = {
+  type: "carousel",
+  perView: 4,
+  gap: 20,
+  autoplay: 2000,
+  breakpoints: {
+    992: {
+      perView: 3,
+    },
+    768: {
+      perView: 2,
+    },
+    576: {
+      perView: 1,
+    },
+  },
+};
+
+new Glide(".glide_1", config_1).mount();
+
+// New Arrivals
+const config_2 = {
+  type: "carousel",
+  perView: 4,
+  gap: 20,
+  autoplay: 2000,
+  breakpoints: {
+    992: {
+      perView: 3,
+    },
+    768: {
+      perView: 2,
+    },
+    576: {
+      perView: 1,
+    },
+  },
+};
+
+new Glide(".glide_2", config_2).mount();
