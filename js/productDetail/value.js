3d72759c438e74d7dc331369fb218d05f8a0a7c1
resul aydın <bm.resulaydin@gmail.com>
2023-06-01 13:59:29 +0300

    gettig clicked size-value and changing its statu to active -thus its focus view changes- have done

--- js/productDetail/value.js
+++ js/productDetail/value.js
@@ -1,3 +1,12 @@
-function valueFunc() {}
+function valueFunc() {
+  const valueList = document.querySelectorAll(".value-list .size-value");
+
+  valueList.forEach((valueItem) => {
+    valueItem.addEventListener("click", function () {
+      valueList.forEach((item) => item.classList.remove("active"));
+      valueItem.classList.add("active");
+    });
+  });
+}
 
 export default valueFunc();
