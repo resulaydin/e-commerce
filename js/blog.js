0ce14f3ec97742efa9ab1a233e26a203c3d8b679
resul aydın <bm.resulaydin@gmail.com>
2023-06-03 23:55:11 +0300

    refactoring has started part-1

---
+++ js/blog.js
@@ -0,0 +1,53 @@
+const blogs = localStorage.getItem("blogs")
+  ? JSON.parse(localStorage.getItem("blogs"))
+  : [];
+
+function blogFunc() {
+  const blogList = document.querySelector(".blog-list");
+
+  let result = "";
+  blogs.forEach((blog) => {
+    result += `
+        <li class="blog-item" >
+                <a href="#" class="blog-image">
+                    <img src="${blog.image}" alt="">
+                </a>
+                <div class="blog-info">
+                    <div class="blog-info-top">
+                    <span>${blog.dateCreated}</span>
+                    -
+                    <span>${blog.commentCount}  Comments</span>
+                    </div>
+                    <div class="blog-info-center ">
+                    <a href="blog-detail.html" class="blog-center-link" data-id="${blog.id}">${blog.headline} </a>
+                    </div>
+                    <div class="blog-info-bottom">
+                    <a href="blog-detail.html" class="blog-bottom-link" data-route-id="${blog.id}" >Read More</a>
+                    </div>
+                </div>
+        </li>
+    `;
+  });
+  blogList.innerHTML = result;
+  blogRoute();
+}
+
+const blogRoute = function () {
+  const blogsCenterRoute = document.querySelectorAll(".blog-center-link");
+  const blogsBottomRoute = document.querySelectorAll(".blog-bottom-link");
+  let blogId = 0;
+
+  blogsCenterRoute.forEach((route) => {
+    route.addEventListener("click", (e) => {
+      blogId = parseInt(e.target.dataset.id);
+      localStorage.setItem("blogId", JSON.stringify(blogId));
+    });
+  });
+  blogsBottomRoute.forEach((route) => {
+    route.addEventListener("click", (e) => {
+      blogId = parseInt(e.target.dataset.routeId);
+      localStorage.setItem("blogId", JSON.parse(blogId));
+    });
+  });
+};
+blogFunc();
